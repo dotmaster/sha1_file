1d72abf00d4d53161329fc387fb6e945fd178c29
Gregor Schwab <isimpl@gmail.com>
2011-03-28 14:43:20 +0200

    now working version

--- test/test.js
+++ test/test.js
@@ -1,7 +1,13 @@
 sha1_file = require('../sha1_file.js')
 util = require('util')
-sha1_file('/Users/dotmaster/Downloads/Software/Microsoft Office 2011 v14.0.0.dmg', function(err, sha1){
-  if (err) util.log(err)
-  else util.log(sha1)
+//var file = '../README.md'
+var file = '/Users/dotmaster/Downloads/Software/Microsoft Office 2011 v14.0.0.dmg'
+stat = require('fs').statSync(file)
+size = stat['size']
+var start = new Date().getTime()
+sha1_file(file, function(err, sha1){
+  if (err) return util.log(err)
+    var end = new Date().getTime()
+  util.log(sha1 + ' rate: ' + (size/(end-start) *1000 /(1024*1024)).toFixed(2) + 'mb/sec')
 })
 
